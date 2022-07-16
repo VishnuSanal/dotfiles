115c43e7c7a1ed5205a05bd948cb5a69ca3abd0f
VishnuSanal <t.v.s10123@gmail.com>
2022-07-16 18:33:10 +0530

    update monitor config

--- .screenlayout/layout.sh
+++ .screenlayout/layout.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP1 --primary --mode 1366x768 --pos 0x0 --rotate normal --output DP1 --off --output HDMI1 --mode 1920x1080 --pos 1366x0 --rotate normal --output HDMI2 --off --output VIRTUAL1 --off
+xrandr --output eDP1 --mode 1366x768 --pos 0x0 --rotate normal --output DP1 --off --output HDMI1 --primary --mode 1920x1080 --pos 1366x0 --rotate normal --output HDMI2 --off --output VIRTUAL1 --off
