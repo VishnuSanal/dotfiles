dc7debb4e5d904bdd8daa8ca9ea84f7f81239cc4
VishnuSanal <t.v.s10123@gmail.com>
2022-10-03 22:59:41 +0530

    update polybar & bspwm

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 
+DIR="$HOME/.config/polybar"
 # Terminate already running bar instances
 killall -q polybar
-# If all your bars have ipc enabled, you can also use 
-# polybar-msg cmd quit
 
-# Launch example bar
-echo "---" | tee -a /tmp/polybar1.log
-polybar example 2>&1 | tee -a /tmp/polybar1.log & disown
-echo "Bars launched..."
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# polybar -q example -c ~/.config/polybar/config & disown
+# Launch the bar
+polybar -q main -c "$DIR"/config.ini &
+# polybar -q main2 -c "$DIR"/config.ini &
