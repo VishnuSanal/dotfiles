7c77b7dc5d78c801544912c5b87936c7992e443b
VishnuSanal <t.v.s10123@gmail.com>
2023-09-02 17:24:34 +0530

    update monitor config with scripts

--- .screenlayout/layout.sh
+++ .screenlayout/layout.sh
@@ -1,2 +1,11 @@
 #!/bin/sh
-xrandr --output eDP-1 --off --output HDMI-1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --off
+
+MODE=$(cat ~/.screenlayout/mode)
+
+# both monitors
+if [ $MODE == 0 ] || [ $MODE == 1 ] || [ $MODE == 4 ] || [ $MODE == 5 ] || [ $MODE == 6 ]; then
+    xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-1 --mode 1920x1080 --pos 1920x0 --rotate normal --output DP-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --off
+# intenral monitor off
+elif [ $MODE == 2 ] || [ $MODE == 3 ]; then
+    xrandr --output eDP-1 --off --output HDMI-1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --off
+fi
--- .screenlayout/monitor.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output eDP-1 --off --output HDMI-1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --off
